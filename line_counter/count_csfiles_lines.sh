b444c38025e772891f1ab33fc2787279a220f04e
Denis Chernushchenko <NightQnEarth@gmail.com>
2020-05-29 09:18:05 +0500

    Little refactored.

--- line_counter/count_csfiles_lines.sh
+++ line_counter/count_csfiles_lines.sh
@@ -1,2 +1,2 @@
 find . -name "*.cs" -not -path "*AssemblyInfo.cs" -not -path "*/obj/*" -not -path "*/bin/*" | xargs wc -l
-read -n 1 -s -r -p "Press any key to exit"
+read -n 1 -s -r -p "Press any key to exit."$'\n'
